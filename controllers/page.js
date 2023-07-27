3aaa48d44eeff3e7464ba67ec9eb669145ccce90
dthcs <dthcsc@gmail.com>
2023-07-27 12:12:41 +0900

    first commit

---
+++ controllers/page.js
@@ -0,0 +1,15 @@
+exports.renderProfile = (req, res) => {
+    res.render('profile', { title: "My Info - SNS" });
+};
+
+exports.renderJoin = (req, res) => {
+    res.render('join', { title: 'Sign-up - SNS'});
+};
+
+exports.renderMain = (req, res, next) => {
+    const twits = [];
+    res.render('main', {
+        title: 'NodeBird',
+        twits,
+    });
+};
