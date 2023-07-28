565afb1cb1fa4802e4706e7cd1ac81120d51ed75
dthcs <dthcsc@gmail.com>
2023-07-28 15:09:37 +0900

    setup database

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
+const { sequelize } = require('./models');
 
 const app = express();
 app.set('port', process.env.PORT || 8000);
@@ -16,6 +17,14 @@ nunjucks.configure('views', {
     express: app,
     watch: true,
 });
+
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('Connect database succesfully');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
